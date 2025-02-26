cdb9987d40cda17b8e662cb5f1234f189b61d268
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2025-02-26 17:28:42 +0100

    ajout d'un commencement de json

--- projet_rust/src/main.rs
+++ projet_rust/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::fs;
+
+#[derive(Serialize,Deserialize,Debug)]
+
+struct Joueur {
+    description:String,
+    nom: String,
+    pronom: String,
+    niveau: String,
+    position:String,
+}
+
+impl Joueur {
+    pub fn new(description: String, nom: String, pronom: String, niveau: String, position: String) -> Joueur {
+        Joueur {
+            description,
+            nom,
+            pronom,
+            niveau,
+            position,
+        }
+    }
+}
+
+fn main(){
+    let data = fs::read_to_string("masterFile.json").unwrap();
+    let joueur: Joueur = serde_json::from_str(&data).unwrap();
+    println!("{}",joueur.description);
 }
