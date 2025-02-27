43963c167c0f5366394c63c4d44e0cda92057c34
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2025-02-27 21:50:39 +0100

    Modification du main pour pouvoir décrire les données du fichier json correctement

--- projet_rust/src/main.rs
+++ projet_rust/src/main.rs
@@ -12,20 +12,40 @@ struct Joueur {
     position:String,
 }
 
-impl Joueur {
-    pub fn new(description: String, nom: String, pronom: String, niveau: String, position: String) -> Joueur {
-        Joueur {
-            description,
-            nom,
-            pronom,
-            niveau,
-            position,
-        }
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct Lieu {
+    ID: String,
+    nom: String,
+    Desc: String,
+    Connections: Vec<Connection>,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Connection {
+    destination: String,
+}
+
 
 fn main(){
     let data = fs::read_to_string("masterFile.json").unwrap();
-    let joueur: Joueur = serde_json::from_str(&data).unwrap();
-    println!("{}",joueur.description);
+    let items: Vec<serde_json::Value> = serde_json::from_str(&data).unwrap();
+    for item in items {
+        match item.get("type").and_then(|t| t.as_str()) {
+            Some("Joueur") => {
+                let joueur: Joueur = serde_json::from_value(item).unwrap();
+                println!("Joueur: {}", joueur.description);
+                println!("Joueur: {}", joueur.position);
+            }
+            Some("lieu") => {
+                let lieu: Lieu = serde_json::from_value(item).unwrap();
+                println!("Lieu: {}", lieu.nom);
+                let connections: Vec<Connection> = lieu.Connections;
+                for connection in connections{
+                    println!("Lieu possibles: {}", connection.destination);
+                }
+            }
+            _ => println!("Type inconnu trouvé dans les données."),
+        }
+        println!("");
+    }
 }
