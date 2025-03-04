f78605c13602f6674c53fa6e2e52cb228330da6d
Grégoire Pierrot <gregpierr453@gmail.com>
2025-03-04 17:23:44 +0100

    Lecture fonctionnel

---
+++ projet_rust/src/JsonManager.rs
@@ -0,0 +1,41 @@
+use crate::structs;
+
+use structs::{Joueur, Lieu};
+
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::fs;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MasterFile {
+    Joueur: Joueur,
+    Lieux: Vec<Lieu>,
+}
+
+pub fn get_joueur() -> Joueur {
+    let data = fs::read_to_string("masterFile.json").unwrap();
+    let master_file : MasterFile = serde_json::from_str(&data).expect("Erreur de parsing");
+    master_file.Joueur
+}
+
+
+pub fn prendre_lieu_id(id: &str) -> Result<Lieu, String> {
+    let data = fs::read_to_string("masterFile.json").unwrap();
+    let master_file: MasterFile = serde_json::from_str(&data).expect("Erreur de parsing");
+    for lieu in master_file.Lieux {
+        if lieu.get_id() == id {
+            return Ok(lieu);
+        }
+    }
+    return Err("Lieu introuvable".to_string());
+}
+
+
+pub fn changer_position_joueur(position: &str) -> Result<(), String> {
+    let data = fs::read_to_string("masterFile.json").unwrap();
+    let mut master_file: MasterFile = serde_json::from_str(&data).expect("Erreur de parsing");
+    master_file.Joueur.set_position(position.to_string());
+    let updated_data = serde_json::to_string_pretty(&master_file).unwrap();// Sauvegarder les données dans le fichier
+    fs::write("masterFile.json", updated_data).unwrap();
+    Ok(())
+}
