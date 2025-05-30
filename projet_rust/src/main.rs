6614c21b5f26e9571de489cc449d2ddc6e5ca405
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2025-05-30 03:56:44 +0200

    Nettoyage

--- projet_rust/src/main.rs
+++ projet_rust/src/main.rs
@@ -22,42 +22,4 @@ fn main() {
     let mut joueur = master_file.get_joueur();
 
     println!("{}",deplacement(&mut master_file,&mut joueur,"piece3"));
-
-    /*joueur.set_position("pièce1".to_string());
-    joueur.set_pronom("il".to_string());
-    println!("{}", joueur);*/
-
-    /*match master_file.changer_nom_joueur("Jane") {
-        Ok(_) => println!("Nom changé"),
-        Err(e) => println!("Erreur lors de la modification du nom joueur : {}", e)
-    }
-
-    match master_file.changer_pronom_joueur("elle") {
-        Ok(_) => println!("Pronom changé"),
-        Err(e) => println!("Erreur lors de la modification pronom du joueur : {}", e)
-    }
-
-    match master_file.changer_niveau_joueur(&9) {
-        Ok(_) => println!("Niveau augmenté"),
-        Err(e) => println!("Erreur lors de la modification du niveau du joueur : {}", e)
-    }
-    
-    match master_file.changer_position_joueur("pièce2") {
-        Ok(_) => println!("Position modifiée"),
-        Err(e) => println!("Erreur lors de la modification de la position du joueur : {}", e)
-    }*/
-
-    /*master_file.sauvegarder(&joueur);
-
-    println!("{}", joueur);
-
-    joueur.set_nom("Jane".to_string());
-    joueur.set_pronom("elle".to_string());
-    joueur.add_niveau(9);
-    joueur.set_position("pièce2".to_string());
-
-    master_file.sauvegarder(&joueur);
-
-    println!("{}", joueur);
-    */
 }
